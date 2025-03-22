41153985e296d66fbec18cddc139ca2473648035
Vinícius <viniciuscarlos087@gmail.com>
2025-03-21 23:33:18 -0300

    feat: add MainPage component and update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+import MainPage from './page/MainPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState<number>(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 3.1415926536)}>
-          + pi ≃ {count.toFixed(2)}
-        </button>
-        <h2>
-          <a href="https://github.com/Viiiniciiiius/viiiniciiiius.github.io"><code>Viiiniciiius GitHub</code></a>
-        </h2>
-      </div>
-    </>
+const Router: React.FC = () => { 
+  return(
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+    </Routes>
   )
 }
+export const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ src/page/MainPage.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+const MainPage:React.FC = () => {
+  const [count, setCount] = useState<number>(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 3.1415926536)}>
+          + pi ≃ {count.toFixed(2)}
+        </button>
+        <h2>
+          <a href="https://github.com/Viiiniciiiius/viiiniciiiius.github.io"><code>Viiiniciiius GitHub</code></a>
+        </h2>
+      </div>
+    </>
+  )
+}
+
+export default MainPage;
